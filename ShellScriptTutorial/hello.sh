33de58ef39b9857f728b35a6eeac4c72d0884d58
dhanarrizky <dhanarrizky23@gmail.com>
2024-07-15 14:04:55 +0700

    createing code for learning shell script

---
+++ ShellScriptTutorial/hello.sh
@@ -0,0 +1,7 @@
+#! /bin/bash #for explation what's this code name we can give some explation using location of sh
+
+# command section using "#"
+
+
+
+echo "hello World"
---
+++ ShellScriptTutorial/variab.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+echo "Test Variable"
+
+echo $BASH
+echo $BASH_VERSION
+echo $HOME
+echo $PWD
+
+
+# createing some variable name
+name=Mark # don't give spaces
+echo The name is $name
+echo this is home : $HOME
